8c8f17da1cdaeb33d265cce3ba8a1d134febf5c3
Boris Paillard <boris.paillard@gmail.com>
2015-01-16 16:35:53 +0100

    add animations

---
+++ source/javascripts/navbar_scroll.js
@@ -0,0 +1,18 @@
+// Navbar transition on scroll
+
+$(function(){
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "rgba(0, 0, 0, 0.7)",
+        "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+         "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+})
